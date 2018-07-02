423968ab518effed0be67b997120a790cb8add33
jfzhou90 <burninghot4jesus2005@gmail.com>
2018-07-02 13:37:25 -0400

    removed cors

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 const fakeData = require('./initial-data/initializaData');
-const cors = require('cors');
 const express = require('express');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys');
+const routes = require('./routes/dialogFlowRoutes')
 
 const app = express();
-app.use(cors);
 app.use(
-    bodyParser.urlencoded({
-      extended: true
-    })
+  bodyParser.urlencoded({
+    extended: true
+  })
 );
 
 app.use(bodyParser.json());
 
 require('./routes/dialogFlowRoutes')(app);
 
-app.listen(process.env.PORT || 8000, function() {
-    fakeData.initializeData();
-    console.log("Server up and listening");
-  });
+
+const PORT = process.env.PORT || 8000
+app.listen(PORT)
+// app.listen(process.env.PORT || 8000, function () {
+//   fakeData.initializeData();
+//   console.log(`Server up and listening on ${process.env.PORT || 8000} `);
+// });
--- routes/dialogFlowRoutes.js
+++ routes/dialogFlowRoutes.js
@@ -1,8 +1,12 @@
 const keys = require('../config/keys');
 
 module.exports = app => {
-  app.post("/api/dialogflow", (request, response) => {
+  app.post('/api/dialogflow', (request, response) => {
     console.log(request);
     return response.send({ speech: "hi", displayText: "hi", source: "webhook-charlie" });
+  }),
+  
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user)
   })
 }
