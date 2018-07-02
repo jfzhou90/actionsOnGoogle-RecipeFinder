6e9e358b1db4202df9747013584b3577f7ddf0e3
jfzhou90 <burninghot4jesus2005@gmail.com>
2018-07-02 13:46:12 -0400

    test

--- routes/dialogFlowRoutes.js
+++ routes/dialogFlowRoutes.js
@@ -2,11 +2,6 @@ const keys = require('../config/keys');
 
 module.exports = app => {
   app.post('/api/dialogflow', (request, response) => {
-    console.log(request);
     return response.send({ speech: "hi", displayText: "hi", source: "webhook-charlie" });
-  }),
-  
-  app.get('/api/current_user', (req, res) => {
-    res.send(req.user)
   })
 }
