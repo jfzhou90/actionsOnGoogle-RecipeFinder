7101d24d67e7581e61c2c9481e0e868d742492f4
jfzhou90 <burninghot4jesus2005@gmail.com>
2018-07-02 14:50:14 -0400

    test

--- routes/dialogFlowRoutes.js
+++ routes/dialogFlowRoutes.js
@@ -2,6 +2,28 @@ const keys = require('../config/keys');
 
 module.exports = app => {
   app.post('/api/dialogflow', (request, response) => {
+    let reply = {
+      "payload": {
+        "google": {
+          "expectUserResponse": true,
+          "richResponse": {
+            "items": [
+              {
+                "simpleResponse": {
+                  "textToSpeech": "Reponse 1"
+                }
+              }
+            ]
+          }
+        }
+      }
+    };
+
+    if(request.actions == "input.queryrecipe") {
+      reply.payload.google.richResponse.items[0].simpleResponse.textToSpeech = "Response 2";
+    }
+
+    
     return response.json({
       "payload": {
         "google": {
