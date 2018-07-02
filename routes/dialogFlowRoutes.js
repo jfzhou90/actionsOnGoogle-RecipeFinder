05f896b1067afc26246a036e4cb0a79b4d1659d2
jfzhou90 <burninghot4jesus2005@gmail.com>
2018-07-02 16:43:00 -0400

    test

--- routes/dialogFlowRoutes.js
+++ routes/dialogFlowRoutes.js
@@ -25,82 +25,51 @@ module.exports = app => {
     if (request.body.queryResult.action == 'input.queryrecipe') {
       // console.log(request.body)
       // reply.payload.google.richResponse.items[0].simpleResponse["textToSpeech"] = "Response 2";
-      reply.payload['google'] = {"expectedInputs": [
-        {
-          "inputPrompt": {
-            "initialPrompts": [
-              {
-                "textToSpeech": "Alright! Here are a few things you can learn. Which sounds interesting?"
-              }
-            ],
-            "noInputPrompts": []
-          },
-          "possibleIntents": [
+      reply = {
+        "conversationToken": "",
+        "expectUserResponse": true,
+        "expectedInputs": [
             {
-              "intent": "actions.intent.OPTION",
-              "inputValueData": {
-                "@type": "type.googleapis.com/google.actions.v2.OptionValueSpec",
-                "listSelect": {
-                  "title": "Things to learn about",
-                  "items": [
-                    {
-                      "optionInfo": {
-                        "key": "MATH_AND_PRIME",
-                        "synonyms": [
-                          "math",
-                          "math and prime",
-                          "prime numbers",
-                          "prime"
-                        ]
-                      },
-                      "title": "Math & prime numbers",
-                      "description": "42 is an abundant number because the sum of its proper divisors 54 is greater…",
-                      "image": {
-                        "url": "http://example.com/math_and_prime.jpg",
-                        "accessibilityText": "Math & prime numbers"
-                      }
-                    },
-                    {
-                      "optionInfo": {
-                        "key": "EGYPT",
-                        "synonyms": [
-                          "religion",
-                          "egpyt",
-                          "ancient egyptian"
-                        ]
-                      },
-                      "title": "Ancient Egyptian religion",
-                      "description": "42 gods who ruled on the fate of the dead in the afterworld. Throughout the under…",
-                      "image": {
-                        "url": "http://example.com/egypt",
-                        "accessibilityText": "Egypt"
-                      }
-                    },
-                    {
-                      "optionInfo": {
-                        "key": "RECIPES",
-                        "synonyms": [
-                          "recipes",
-                          "recipe",
-                          "42 recipes"
-                        ]
-                      },
-                      "title": "42 recipes with 42 ingredients",
-                      "description": "Here's a beautifully simple recipe that's full of flavor! All you need is some ginger and…",
-                      "image": {
-                        "url": "http://example.com/recipe",
-                        "accessibilityText": "Recipe"
-                      }
+                "inputPrompt": {
+                    "richInitialPrompt": {
+                        "items": [
+                            {
+                                "simpleResponse": {
+                                    "textToSpeech": "Math and prime numbers it is!"
+                                }
+                            },
+                            {
+                                "basicCard": {
+                                    "title": "Math & prime numbers",
+                                    "formattedText": "42 is an even composite number. It\n    is composed of three distinct prime numbers multiplied together. It\n    has a total of eight divisors. 42 is an abundant number, because the\n    sum of its proper divisors 54 is greater than itself. To count from\n    1 to 42 would take you about twenty-one…",
+                                    "image": {
+                                        "url": "https://example.google.com/42.png",
+                                        "accessibilityText": "Image alternate text"
+                                    },
+                                    "buttons": [
+                                        {
+                                            "title": "Read more",
+                                            "openUrlAction": {
+                                                "url": "https://example.google.com/mathandprimes"
+                                            }
+                                        }
+                                    ],
+                                    "imageDisplayOptions": "CROPPED"
+                                }
+                            }
+                        ],
+                        "suggestions": []
                     }
-                  ]
-                }
-              }
+                },
+                "possibleIntents": [
+                    {
+                        "intent": "actions.intent.TEXT"
+                    }
+                ]
             }
-          ]
-        }
-      ]
+        ]
     }
-  }
+    }
 
 
     return response.json(reply);
