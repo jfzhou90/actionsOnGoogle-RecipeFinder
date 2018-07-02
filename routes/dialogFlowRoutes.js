40d25c4fc08f7d4b0bd9996a21b480c2fa227abd
jfzhou90 <burninghot4jesus2005@gmail.com>
2018-07-02 16:10:37 -0400

    test

--- routes/dialogFlowRoutes.js
+++ routes/dialogFlowRoutes.js
@@ -1,3 +1,5 @@
+// request.body.originalDetectIntentRequest.payload.user.userId
+
 const keys = require('../config/keys');
 
 module.exports = app => {
@@ -20,7 +22,7 @@ module.exports = app => {
     };
 
     if (request.body.queryResult.action == 'input.queryrecipe') {
-      console.log(request.body.originalDetectIntentRequest.payload.user);
+      console.log(request.body.originalDetectIntentRequest.payload.user.userId);
       reply.payload.google.richResponse.items[0].simpleResponse["textToSpeech"] = "Response 2";
     }
 
