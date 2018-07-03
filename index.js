5f48361edc5da03a83e14ae9b05a6e8af593ebf1
jfzhou90 <burninghot4jesus2005@gmail.com>
2018-07-02 20:58:38 -0400

    test

--- index.js
+++ index.js
@@ -3,42 +3,39 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 const routes = require('./routes/dialogFlowRoutes')
+const { dialogflow, Image } = require('actions-on-google')
 
-// const app = express();
-// app.use(
-//   bodyParser.urlencoded({
-//     extended: true
-//   })
-// );
+//server side files.
+const app = express();
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
-// app.use(bodyParser.json());
+app.use(bodyParser.json());
 
-// app.use(function(request, response, next) {
-//   response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, X-Authentication");
-//   response.header("Access-Control-Allow-Origin", "*");
+app.use(function(request, response, next) {
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, X-Authentication");
+  response.header("Access-Control-Allow-Origin", "*");
   
-//   // Pre-flight Request
-//   if ('OPTIONS' == request.method) {
-//       return response.status(200).send();
-//   }
+  // Pre-flight Request
+  if ('OPTIONS' == request.method) {
+      return response.status(200).send();
+  }
 
-//   next();
-// });
+  next();
+});
 
-// require('./routes/dialogFlowRoutes')(app);
-
-const {
-  dialogflow,
-  Image,
-} = require('actions-on-google')
+require('./routes/dialogFlowRoutes')(app);
 
 // Create an app instance
 
-const app = dialogflow()
+const googleflow = dialogflow();
 
 // Register handlers for Dialogflow intents
 
-app.intent('Default Welcome Intent', conv => {
+googleflow.intent('Default Welcome Intent', conv => {
   conv.ask('Hi, I\'m Charlie, I\'m your recipe buddy. What shall we cook today?')
   conv.ask(new Image({
     url: 'https://cdn.pixabay.com/photo/2016/01/10/18/59/charlie-brown-1132276_960_720.jpg',
@@ -47,15 +44,15 @@ app.intent('Default Welcome Intent', conv => {
 })
 
 // Intent in Dialogflow called `Goodbye`
-app.intent('Goodbye', conv => {
+googleflow.intent('Goodbye', conv => {
   conv.close('See you later!')
 })
 
-app.intent('Default Fallback Intent', conv => {
+googleflow.intent('Default Fallback Intent', conv => {
   conv.ask(`I didn't understand. Can you tell me something else?`)
 })
 
-express().use(bodyParser.json(), app).listen(process.env.PORT || 8000, function () {
+express().use(bodyParser.json(), googleflow).listen(process.env.PORT || 8000, function () {
   fakeData.initializeData();
   console.log(`Server up and listening on ${process.env.PORT || 8000} `);
 });
