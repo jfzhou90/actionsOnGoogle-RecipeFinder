b6f99e1960fce07f4b19959c105efbe7a9debbc3
jfzhou90 <burninghot4jesus2005@gmail.com>
2018-07-02 22:27:44 -0400

    test

--- index.js
+++ index.js
@@ -70,38 +70,6 @@ googleflow.intent('Query Recipe', conv => {
 
   conv.ask(`Here are some of recipes about ${conv.body.queryResult.parameters.food}`);
   // Create a carousel
-  conv.ask(new Carousel({
-    items: {
-      // Add the first item to the carousel
-      "A": {
-        synonyms: [
-          'synonym of title 1',
-          'synonym of title 2',
-          'synonym of title 3',
-        ],
-        title: 'Title of First Carousel Item',
-        description: 'This is a description of a carousel item.',
-        image: new Image({
-          url: 'https://cdn.pixabay.com/photo/2016/01/10/18/59/charlie-brown-1132276_960_720.jpg',
-          alt: 'Image alternate text',
-        }),
-      },
-      // Add the second item to the carousel
-      "B": {
-        synonyms: [
-          'Google Home Assistant',
-          'Assistant on the Google Home',
-        ],
-        title: 'Google Home',
-        description: 'Google Home is a voice-activated speaker powered by ' +
-          'the Google Assistant.',
-        image: new Image({
-          url: 'https://cdn.pixabay.com/photo/2016/01/10/18/59/charlie-brown-1132276_960_720.jpg',
-          alt: 'Google Home',
-        })
-      }
-    }
-  }));
 })
 
 // Intent in Dialogflow called `Goodbye`
