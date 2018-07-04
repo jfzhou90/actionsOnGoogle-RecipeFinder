96d7d218caf812869139a3925051bfed5680048f
jfzhou90 <burninghot4jesus2005@gmail.com>
2018-07-03 22:43:48 -0400

    test

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ googleflow.intent('Query Recipe', async conv => {
 
   // Replace this with fake data with request call when app is ready for deployment
   await request.get(options, (error, response, body) => {
-    if(body.results.length != 0){
+    if(body.results){
       searchResult = body;
       console.log("1" + searchResult);
     }
