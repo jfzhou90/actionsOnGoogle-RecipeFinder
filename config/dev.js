df875c0fe8919e2a03ce5264ba7f941756b4747e
Joey Zhou <burninghot4jesus2005@gmail.com>
2018-07-05 12:13:40 -0400

    Delete dev.js

--- config/dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  cookieKey: 'ProjectShiftCookies',
-  mongoURI: 'mongodb://localhost/actionongoogle-recipe',
-  apiKey: 'kVM7CT6wCYmshTU7EIozWKueIIuvp1T4lifjsn8lzCKEbe9W7d',
-  apiHost: 'spoonacular-recipe-food-nutrition-v1.p.mashape.com'
-}
